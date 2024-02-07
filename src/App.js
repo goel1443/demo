2a52a8e5ca0bd27bc730b4baf205ba9bc8613a2f
goel1443 <goel1443@gmail.com>
2024-02-07 22:23:47 +0530

    modiefied data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
+  let a={
+    name:'john',
+    email:"john@gmail.com"
+  }
   return (
     <div className="App">
-     hello world
+  
+
+     <Header info={a} >
+      <h3>hello</h3>
+     </Header>
+
     </div>
   );
 }
