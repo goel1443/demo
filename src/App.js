ad590ebc390a0c8cb39e5902b68cd206b339f9c0
goel1443 <goel1443@gmail.com>
2024-02-08 21:56:22 +0530

    fontawesome work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
+import Card from 'react-bootstrap/Card';
+import { Container, Row,Button } from 'react-bootstrap';
+import { fake } from './Data/Fake';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
   let a={
@@ -9,14 +16,37 @@ function App() {
   }
   return (
     <div className="App">
-  
+      <Container>
+        <Row>
+            <FontAwesomeIcon icon={faWhatsapp} className='text-danger '/> 
+                    
+            <FontAwesomeIcon icon={faFacebook}/> 
+            <FontAwesomeIcon icon={faPhone}/> 
 
-     <Header info={a} >
-      <h3>hello</h3>
-     </Header>
+        </Row>
+      </Container>
+      
+
 
     </div>
   );
 }
 
 export default App;
+
+function ChildCardItem({item}){
+  return (
+    <div className="col-3 mb-4">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{item.id} {item.title}</Card.Title>
+        <Card.Text>
+          {item.body}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+ function Header({info,children}) {
+   console.log(children)
+  return (
+      <div>
+        {children}
+      <h1>welcome to user</h1> 
+  
+    </div>
+  )
+}
+export default Header
